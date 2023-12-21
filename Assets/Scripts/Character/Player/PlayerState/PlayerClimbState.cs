ecd276c5a75028a7e766ed50ac20c0c8c002ca82
sato <shatang821@gmail.com>
2023-12-21 12:59:24 +0900

    ClimbDone

--- Assets/Scripts/Character/Player/PlayerState/PlayerClimbState.cs
+++ Assets/Scripts/Character/Player/PlayerState/PlayerClimbState.cs
@@ -9,6 +9,7 @@ public class PlayerClimbState : PlayerState
     [Header("Climb info")]
     [SerializeField] float gravity = 0;
     [SerializeField] float climbSpeed = 4;
+    int jumpDir;
     public override void Enter()
     {
         base.Enter();
@@ -25,12 +26,28 @@ public class PlayerClimbState : PlayerState
     public override void LogicUpdate()
     {
         base.LogicUpdate();
+
+        jumpDir = (player.facingDir == xInput || xInput == 0) ? 1 : -1;
+
+        player.anim.SetFloat("playerFacing", jumpDir);
+
         if(!Climb)
             stateMachine.SwitchState(typeof(PlayerIdleState));
         if (player.IsWallDetected() && xInput == player.facingDir && !Climb)
             stateMachine.SwitchState(typeof(PlayerWallSlideState));
         if (Jump && player.IsWallDetected())
-            stateMachine.SwitchState(typeof(PlayerWallJumpState));
+        {
+            if(jumpDir == -1)
+            {
+                player.Flip();
+                stateMachine.SwitchState(typeof(PlayerWallJumpState));
+            }
+            else
+                stateMachine.SwitchState(typeof(PlayerWallJumpState));
+        }
+        if (!player.IsWallDetected())
+            stateMachine.SwitchState(typeof(PlayerFallState));
+            
 
     }
 
