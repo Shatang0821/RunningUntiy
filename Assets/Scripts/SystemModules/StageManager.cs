e9fc399a34e263511ee76eb4e569838f0f400760
Sato <shatang821@gmail.com>
2024-01-10 22:44:09 +0900

    Stage初期化部分と最適化の途中

---
+++ Assets/Scripts/SystemModules/StageManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+    [SerializeField] private List<GameObject>stages = new List<GameObject>();
+
+    private GameObject currentStage;
+    private int currentIndex = 0;
+    [SerializeField] bool isUseManager;
+    private void Start()
+    {
+        if(isUseManager)
+            Initialize();
+    }
+
+    private void Initialize()
+    {
+        SetStages();
+        // 現在のステージをアクティブにする
+        currentStage.SetActive(true);
+        stages[1].SetActive(true);
+  
+    }
+    
+    /// <summary>
+    /// 子オブジェクトにあるステージをリストに追加
+    /// </summary>
+    void SetStages()
+    {
+        foreach(Transform child in transform) 
+        { 
+            stages.Add(child.gameObject);
+            child.gameObject.SetActive(false);
+        }
+        currentStage = stages[currentIndex];
+    }
+
+
+}
