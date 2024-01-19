ee085a84ae0f2cbff6c8ed1ea88c86be1fd41b80
sato <shatang821@gmail.com>
2024-01-19 11:13:41 +0900

    Saw移動の最適化

--- Assets/Scripts/Miscs/PatrolPointsMover.cs
+++ Assets/Scripts/Miscs/PatrolPointsMover.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
+
+
+
 public class PatrolPointsMover : MonoBehaviour
 {
+    private enum PatrolType
+    {
+        Loop,     // 1234, 1234...
+        PingPong  // 1234321, 1234321...
+    }
+    [Header("Move Info")]
+    [SerializeField] private PatrolType patrolType; // 列挙型を使う
+
     [SerializeField] public List<Transform> points;
     [SerializeField] private float moveSpeed;
+    
     private int pointIndex = 0;
-
     private bool isGoingForward = true; //ポイントに向かって前進しているかを追跡
+
     private void Start()
     {
         if(points.Count > 0)
@@ -30,29 +42,28 @@ public class PatrolPointsMover : MonoBehaviour
                 yield return null;
             }
 
-            // 更新: ポイントインデックスを更新するロジック
-            if (isGoingForward)
-            {
-                if (pointIndex >= points.Count - 1)
+            UpdatePointIndex();
+        }
+    }
+
+    /// <summary>
+    /// 動きパターン制御
+    /// </summary>
+    private void UpdatePointIndex()
+    {
+        switch (patrolType)
+        {
+            case PatrolType.Loop:
+                pointIndex = (pointIndex + 1) % points.Count;
+                break;
+
+            case PatrolType.PingPong:
+                // pointIndex が増加または減少するかを決定
+                if ((pointIndex >= points.Count - 1 && isGoingForward) || (pointIndex <= 0 && !isGoingForward))
                 {
-                    isGoingForward = false;
+                    isGoingForward = !isGoingForward; // 方向を反転
                 }
-                else
-                {
-                    pointIndex++;
-                }
-            }
-            else
-            {
-                if (pointIndex <= 0)
-                {
-                    isGoingForward = true;
-                }
-                else
-                {
-                    pointIndex--;
-                }
-            }
+                break;
         }
     }
 
